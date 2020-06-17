e352bb456c7294c403530673eefe467ea86d5124
Aria-Afz <66949489+Aria-Afz@users.noreply.github.com>
2020-06-17 16:37:32 +0430

    element correction2

--- src/Element.java
+++ src/Element.java
@@ -19,19 +19,20 @@ class Element {
 	public double getVoltage(int cycle) { return storedVoltages.get(cycle); }
 
 	public double getCurrent(int cycle, int dt) {
-		if(name.charAt(0) == 'R')
-			return getVoltage(cycle) / value;
-		else if(name.charAt(0) == 'I')
-			return value;
-		else if(name.charAt(0) == 'L') {
-			double i = 0;
-			for(int j = 0; j < cycle; j++)
-				i += getVoltage(j);
-			return i / value;
-		} else if (name.charAt(0) == 'C') {
-			if (cycle == 0)
-				return 0;
-			return (getVoltage(cycle) - getVoltage(cycle - 1)) / dt;
+		switch (name.charAt(0)) {
+			case 'R':
+				return getVoltage(cycle) / value;
+			case 'I':
+				return value;
+			case 'L':
+				double i = 0;
+				for(int j = 0; j < cycle; j++)
+					i += getVoltage(j);
+				return i / value;
+			case 'C':
+				if (cycle == 0)
+					return 0;
+				return (getVoltage(cycle) - getVoltage(cycle - 1)) / dt;
 		}
 		return 0;
 	}
