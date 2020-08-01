c9f6d1f0d31d2e260f1500eb97d890f02a55699f
Aria-Afz <66949489+Aria-Afz@users.noreply.github.com>
2020-08-01 19:02:54 +0430

    circuit test vol 1.0

--- src/Circuit.java
+++ src/Circuit.java
@@ -34,35 +34,55 @@ public class Circuit {
 			exit(-4);
 	}
 
-	void unionCheck() {
-		for (Node e : allNodes.values())
-			e.visited = false;
-		HashSet<Node> visited = new HashSet<>();
-		visited.add(allNodes.get((byte) 0));
-		while (visited.size() != allNodes.size())
-			for (Node i : visited) {
-				for (Node j : i.nodeNeighbours)
-					j.visited = true;
-				for (Element ele : i.elementNeighbours) {
-					char k = ele.getName().charAt(0);
-					if (k == 'V' || k == 'E' || k == 'H') {
-						if (ele.getPositiveNode() == i)
-							ele.getNegativeNode().union = i.union;
-						else
-							ele.getPositiveNode().union = i.union;
-					}
-				}
-			}
-		for (Node e : allNodes.values()) {
-			if (allUnions.containsKey(e.union))
-				allUnions.get(e.union).nodes.add(e);
-			else
-				allUnions.put(e.union, new Union(e));
-		}
-	}
+//	void unionCheck() {
+//		for (Node e : allNodes.values())
+//			e.visited = false;
+//		HashSet<Node> visited = new HashSet<>();
+//		visited.add(allNodes.get((byte) 0));
+//		while (visited.size() != allNodes.size())
+//			for (Node i : visited) {
+//				for (Node j : i.nodeNeighbours)
+//					j.visited = true;
+//				for (Element ele : i.elementNeighbours) {
+//					char k = ele.getName().charAt(0);
+//					if (k == 'V' || k == 'E' || k == 'H') {
+//						if (ele.getPositiveNode() == i)
+//							ele.getNegativeNode().union = i.union;
+//						else
+//							ele.getPositiveNode().union = i.union;
+//					}
+//				}
+//			}
+//		for (Node e : allNodes.values()) {
+//			if (allUnions.containsKey(e.union))
+//				allUnions.get(e.union).nodes.add(e);
+//			else
+//				allUnions.put(e.union, new Union(e, e.union));
+//		}
+//	}
+//
+//	void run() {
+//		unionCheck();
+//		for (int i = 1; i <= time / dt; i++) {
+//			for (Node e : allNodes.values())
+//				e.storedVoltages.add(e.storedVoltages.get(i - 1));
+//			for (Union e : allUnions.values())
+//				if (e.getName() != 0)
+//					solve(e, i);
+//			for (Element ele : allElements.values())
+//				ele.update(i, dt);
+//			for (Union e : allUnions.values())
+//				e.update(i, dt);
+//		}
+//		printResult();
+//	}
+//
+//	void solve(Union e, int cycle) {
+//
+//	}
 
 	void run() {
-		unionCheck();
+		//unionCheck();
 		for (int i = 1; i <= time / dt; i++) {
 			for (Node e : allNodes.values())
 				e.storedVoltages.add(e.storedVoltages.get(i - 1));
@@ -83,7 +103,6 @@ public class Circuit {
 		e.storedVoltages.remove(cycle);
 		e.storedVoltages.add(v + dv);
 		double kcl2 = kclCalculate(e, cycle);
-		System.out.println(kcl1 + " " + kcl2);
 		e.storedVoltages.remove(cycle);
 		e.storedVoltages.add(v + (Math.abs(kcl1) - Math.abs(kcl2)) * dv / di);
 	}
@@ -126,25 +145,16 @@ public class Circuit {
 
 	public double getTime() { return time; }
 
-	public void setDt(double dt, int n) {
-		if (dt == 0)
-			exit(n);
-		else
-			this.dt = dt;
+	public void setDt(double dt) {
+		this.dt = dt;
 	}
 
-	public void setDv(double dv, int n) {
-		if (dv == 0)
-			exit(n);
-		else
-			this.dv = dv;
+	public void setDv(double dv) {
+		this.dv = dv;
 	}
 
-	public void setDi(double di, int n) {
-		if (di == 0)
-			exit(n);
-		else
-			this.di = di;
+	public void setDi(double di) {
+		this.di = di;
 	}
 
 	public int getCycle(double t) { return (int) Math.round(t/dt); }
--- src/Union.java
+++ src/Union.java
@@ -3,10 +3,24 @@ import java.util.ArrayList;
 public class Union {
     byte name;
     ArrayList<Node> nodes = new ArrayList<>();
+    Node main;
     double voltage = 0;
 
-    Union(Node e) {
-        nodes.add(e);
+    Union(Node main, byte name) {
+        nodes.add(main);
+        this.name = name;
+        this.main = main;
     }
 
+    public void update(int i, double dt) {
+//        for (Node e : nodes) {
+//            if (main == e)
+//                main.storedVoltages.add(voltage);
+//            else
+//                for (Element ele : e.elementNeighbours)
+//
+//        }
+    }
+
+    public byte getName() { return name; }
 }
